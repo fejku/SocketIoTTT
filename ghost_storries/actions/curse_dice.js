34516a203e3b7baf33e75dc2f9aa594b8d2eb11f
Unknown <przecinek45@o2.pl>
2018-03-02 23:18:57 +0100

    fixes for lint

--- ghost_storries/actions/curse_dice.js
+++ ghost_storries/actions/curse_dice.js
@@ -1,70 +1,84 @@
 const CircleOfPrayer = require('../villagers/circle_of_prayer');
 
 function loseQi(players, bank, circleOfPrayer) {
-    players.getActualPlayer().loseQi();
-    bank.updateMarkers(players.getTaoists(), circleOfPrayer);
+  players.getActualPlayer().loseQi();
+  bank.updateMarkers(players.getTaoists(), circleOfPrayer);
 }
 
 function loseAllTaoTokens(players, bank, circleOfPrayer) {
-    players.getActualPlayer().loseAllTaoMarkers();
-    bank.updateMarkers(players.getTaoists(), circleOfPrayer);
+  players.getActualPlayer().loseAllTaoMarkers();
+  bank.updateMarkers(players.getTaoists(), circleOfPrayer);
 }
 
 function getTilePositionToHaunt(ghostPosition, villagers, step = 0) {
-    switch (ghostPosition.boardIndex) {
-        case 0:
-            const tilePosition = ghostPosition.fieldIndex + 3 * step;
-            break;
-        case 1:
-            const tilePosition = 3 * ghostPosition.fieldIndex + 2 - step;
-            break;
-        case 2:
-            const tilePosition = 6 + ghostPosition.fieldIndex - 3 * step;
-            break;
-        case 3:
-            const tilePosition = 3 * ghostPosition.fieldIndex + step;
-            break;
-    }
-    if (villagers.getVillager(tilePosition).isHaunted())
-        getTilePositionToHaunt(ghostPosition, villagers, ++step)
-    else
-        return tilePosition;
+  let tilePosition = -1;
+
+  switch (ghostPosition.boardIndex) {
+    case 0:
+      tilePosition = ghostPosition.fieldIndex + (3 * step);
+      break;
+    case 1:
+      tilePosition = (3 * ghostPosition.fieldIndex) + (2 - step);
+      break;
+    case 2:
+      tilePosition = (6 + ghostPosition.fieldIndex) - (3 * step);
+      break;
+    case 3:
+      tilePosition = (3 * ghostPosition.fieldIndex) + step;
+      break;
+    default:
+      break;
+  }
+
+  if (villagers.getVillager(tilePosition).isHaunted()) {
+    getTilePositionToHaunt(ghostPosition, villagers, ++step); // eslint-disable-line no-param-reassign, max-len
+  }
+
+  return tilePosition;
 }
 
 function hauntTile(playerPosition, ghostPosition, villagers, isCemeteryCall) {
-    if (isCemeteryCall) {
-        villagers.getVillager(playerPosition).setHaunted(true);
-    } else {
-        const tilePositionToHaunt = getTilePositionToHaunt(ghostPosition, villagers);
-        villagers.getVillager(tilePositionToHaunt).setHaunted(true);
-    }
+  if (isCemeteryCall) {
+    villagers.getVillager(playerPosition).setHaunted(true);
+  } else {
+    const tilePositionToHaunt = getTilePositionToHaunt(ghostPosition, villagers);
+    villagers.getVillager(tilePositionToHaunt).setHaunted(true);
+  }
 }
 
 function throwCurseDice(socket, board, players, ghostPosition, bank, isCemeteryCall) {
-    const throwResult = Math.floor(Math.random() * 6);
-    switch (throwResult) {
-        //(0-1) No effect.
-        //The first active village tile in front of the ghost becomes haunted.
-        //Q : When I use the Cemetery, if “Haunt Tile” is rolled on the Curse Die, which tile becomes haunted?
-        //A : The Cemetery itself.
-        case 2:
-            hauntTile(players.getActualPlayer().getPosition(), ghostPosition, board.getVillagers(), isCemeteryCall);
-            break;
-        //The player must bring a ghost into play according to the placement rules.
-        case 3:
-            board.ghostArrival(socket, players, bank, board.getVillagerByClass(CircleOfPrayer))
-            break;
-        //The player must discard all his Tao tokens.            
-        case 4:
-            loseAllTaoTokens(players, bank, board.getVillagerByClass(CircleOfPrayer))
-            break;
-        //The Taoist loses one Qi token.            
-        case 5:
-            loseQi(players, bank, board.getVillagerByClass(CircleOfPrayer));
-            break;
-    }
+  const throwResult = Math.floor(Math.random() * 6);
+  switch (throwResult) {
+    // (0-1) No effect.
+    // The first active village tile in front of the ghost becomes haunted.
+    // Q : When I use the Cemetery, if “Haunt Tile” is rolled on the Curse Die,
+    //     which tile becomes haunted?
+    // A : The Cemetery itself.
+    case 2:
+      hauntTile(
+        players.getActualPlayer().getPosition(),
+        ghostPosition, board.getVillagers(), isCemeteryCall,
+      );
+      break;
+      // The player must bring a ghost into play according to the placement rules.
+    case 3:
+      board.ghostArrival(socket, players, bank, board.getVillagerByClass(CircleOfPrayer));
+      break;
+      // The player must discard all his Tao tokens.
+    case 4:
+      loseAllTaoTokens(players, bank, board.getVillagerByClass(CircleOfPrayer));
+      break;
+      // The Taoist loses one Qi token.
+    case 5:
+      loseQi(players, bank, board.getVillagerByClass(CircleOfPrayer));
+      break;
+    default:
+      break;
+  }
 }
 
-module.exports.throwCurseDice = (socket, board, ghostPosition, players, bank) => throwCurseDice(socket, board, players, ghostPosition, bank, false);
+module.exports.throwCurseDice = (socket, board, ghostPosition, players, bank) =>
+  throwCurseDice(socket, board, players, ghostPosition, bank, false);
 
-module.exports.throwCurseDiceCemetry = (socket, board, players, bank) => throwCurseDice(socket, board, players, null, bank, true);
+module.exports.throwCurseDiceCemetry = (socket, board, players, bank) =>
+  throwCurseDice(socket, board, players, null, bank, true);
--- ghost_storries/villagers/cemetery.js
+++ ghost_storries/villagers/cemetery.js
@@ -3,43 +3,46 @@ const CircleOfPrayer = require('./circle_of_prayer');
 const dice = require('../actions/curse_dice');
 
 
-//Pośród porosłych chwastami nagrobków grabarz strzeże drzwi pomiędzy królestwami.
-//Przywróć zmarłego Taoistę do rozgrywki. Daj mu 2 Czi, następnie rzuć kością Klątwy. 
+// Pośród porosłych chwastami nagrobków grabarz strzeże drzwi pomiędzy królestwami.
+// Przywróć zmarłego Taoistę do rozgrywki. Daj mu 2 Czi, następnie rzuć kością Klątwy.
 class Cemetery extends Villager {
-    constructor() {
-        super();
-        this.name = 'Cemetery'
+  constructor() {
+    super();
+    this.name = 'Cemetery';
+  }
+
+
+  validateHelp(board, players, bank) {
+    // Check if there are enough Qi markers in bank
+    if (bank.getQiMarkers(players.getTaoists()) < 2) {
+      return false;
     }
 
-    
-    validateHelp(board, players, bank) {
-        //Check if there are enough Qi markers in bank
-        if (bank.getQiMarkers(players.getTaoists()) < 2)
-            return false;
-
-        //Check if there is any dead taoist
-        for (const taoist of players.getTaoists())
-            if (!taoist.isAlive())
-                return true;
-        return false;
+    // Check if there is any dead taoist
+    for (const taoist of players.getTaoists()) {
+      if (!taoist.isAlive()) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    async _pickPlayerToReview(socket, players) {
-        return new Promise((resolve, reject) => {
-            const deadPlayers = players.getDeadPlayers();
-            socket.emit('ghost pick player to review', deadPlayers, pickedPlayerColor => {
-                const player = players.getPlayerByColor(pickedPlayerColor);
-                resolve(player);
-            });
-        });
-    }
+  async pickPlayerToReview(socket, players) {
+    return new Promise((resolve) => {
+      const deadPlayers = players.getDeadPlayers();
+      socket.emit('ghost pick player to review', deadPlayers, (pickedPlayerColor) => {
+        const player = players.getPlayerByColor(pickedPlayerColor);
+        resolve(player);
+      });
+    });
+  }
 
-    async action(socket, board, players, bank) {
-        const player = await this._pickPlayerToReview(socket, players);
-        player.gainQi(2);
-        bank.updateMarkers(players.getTaoists(), board.getVillagerByClass(CircleOfPrayer));
-        dice.throwCurseDiceCemetry(socket, board, players, bank);
-    }
+  async action(socket, board, players, bank) {
+    const player = await this.pickPlayerToReview(socket, players);
+    player.gainQi(2);
+    bank.updateMarkers(players.getTaoists(), board.getVillagerByClass(CircleOfPrayer));
+    dice.throwCurseDiceCemetry(socket, board, players, bank);
+  }
 }
 
 module.exports = Cemetery;
