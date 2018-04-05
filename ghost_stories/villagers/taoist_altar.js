88f0bbed6a88a723a6508323233b970bd0ec4eb0
Krystian <krystian.kalita@zeto.olsztyn.pl>
2018-04-05 09:28:24 +0200

    fixed taoist altar

--- ghost_stories/villagers/taoist_altar.js
+++ ghost_stories/villagers/taoist_altar.js
@@ -1,5 +1,8 @@
 const Villager = require('./villager');
 
+const UI = require('../utils/UI');
+const questions = require('../utils/questionsUI');
+
 // Nullify the haunting of one Village tile by turning its active side face-up, and then bring a ghost into play.
 class TaoistAltar extends Villager {
   constructor() {
@@ -7,49 +10,31 @@ class TaoistAltar extends Villager {
     this.name = 'Taoist Altar';
   }
 
-  validateHelp(board, players, bank) {
+  validateHelp(board, players, bank) { /* eslint-disable-line no-unused-vars */
     if (!super.validateHelp()) {
       return false;
     }
     // Check if exists haunted tile
-    return board.getAllVillagers().find(item => item.isHaunted()) !== undefined;
+    return board.getAllVillagers().find(villager => villager.isHaunted()) !== undefined;
   }
 
   getHauntedTiles(villagers) {
-    return villagers.filter(item => item.isHaunted()).map(item => item.name);
-  }
-
-  validatePickedTile(hauntedTiles, pickedTile) {
-    return hauntedTiles.find(item => item.name === pickedTile.name) !== undefined;
-  }
-
-  pickTile(socket, hauntedTiles) {
-    return new Promise((resolve, reject) => {
-      console.log('Taoist altar hauntedTiles: ', hauntedTiles);
-      socket.emit(
-        'ghost question',
-        'Pick village tile from which you want to chase ghost away',
-        hauntedTiles,
-        null,
-        (pickedTile) => {
-          console.log('Taoist altar pickedTile: ', pickedTile);
-          if (this.validatePickedTile(hauntedTiles, pickedTile)) {
-            resolve(pickedTile);
-          } else {
-            reject();
-          }
-        },
-      );
-    });
+    return villagers.reduce((prev, curr, index) => {
+      if (curr.isHaunted()) {
+        prev.push(index);
+      }
+      return prev;
+    }, []);
   }
 
   async action(socket, board, players, bank) {
     // Get haunted tiles
     const hauntedTiles = this.getHauntedTiles(board.getAllVillagers());
     // Pick tile
-    const villagerName = await this.pickTile(socket, hauntedTiles);
+    const villagerId = await questions.pickVillagerTile(socket, hauntedTiles);
     // Cancel haunting
-    board.getVillagers().getVillagerByName(villagerName).setHaunted(false);
+    board.getVillager(villagerId).setHaunted(false);
+    UI.refreshVillagers(socket, board.getAllVillagers());
     // Get new ghost into play
     await board.ghostArrival(socket, players, bank);
   }
